a9dac408766b2f1efbdd25af8c94f94ac8b647a1
peli <pelikhan@users.noreply.github.com>
2020-09-11 02:54:31 -0700

    handle save messages

--- src/widget.ts
+++ src/widget.ts
@@ -2,10 +2,11 @@ import { IFileBrowserFactory } from '@jupyterlab/filebrowser';
 import { IFrame } from "@jupyterlab/apputils";
 import { Contents, ContentsManager } from '@jupyterlab/services';
 import { toArray } from '@lumino/algorithm';
+import { PathExt } from '@jupyterlab/coreutils'
 
 export interface IMessage {
     id?: string;
-    source: 'jacdac' | 'host',
+    source?: 'jacdac' | 'host',
     type: string;
     data: any;
     requireAck?: boolean;
@@ -15,7 +16,16 @@ export interface IAckMessage extends IMessage {
     ackId?: string;
     data: {
         status: "success" | "error";
-        error: any;
+        data?: any;
+        error?: any;
+    }
+}
+export interface IStatusMessage extends IMessage {
+    type: 'status',
+    data: {
+        status: 'ready' | 'error',
+        data?: any;
+        error?: any;
     }
 }
 export interface ISaveTextMessage extends IMessage {
@@ -45,19 +55,34 @@ export class JACDACWidget extends IFrame {
         this.iframe.setAttribute('allow', 'usb')
 
         this.options.url = this.options?.url || 'https://microsoft.github.io/jacdac-ts';
+
+        if (/jddbg=1/.test(window.location.href))
+            this.options.url = 'http://localhost:8000'
+
+        this.options.url = PathExt.removeSlash(this.options.url)
         this.url = `${this.options.url}/tools/collector`
 
-        this.iframe.addEventListener('message', ev => this.handleMessage((ev as MessageEvent).data), false)
+        this.handleMessage = this.handleMessage.bind(this)
+        window.addEventListener('message', this.handleMessage, false)
+    }
+
+    dispose() {
+        super.dispose();
+        window.removeEventListener('message', this.handleMessage)
     }
 
     get iframe(): HTMLIFrameElement {
         return this.node.querySelector('iframe');
     }
 
-    private handleMessage(msg: IMessage) {
+    private handleMessage(ev: Event) {
+        const msg = (ev as MessageEvent).data;
         if (msg?.source !== 'jacdac') return;
         try {
             switch (msg.type) {
+                case 'status':
+                    this.handleStatusMessage(msg as IStatusMessage)
+                    break;
                 case 'save-text':
                     this.handleSaveTextMessage(msg as ISaveTextMessage)
                     break;
@@ -66,15 +91,22 @@ export class JACDACWidget extends IFrame {
                     break;
             }
         } catch (e) {
-            this.sendAck(msg, {
+            this.sendAck(msg, undefined, {
                 message: e.message
             })
         }
     }
 
+    private handleStatusMessage(msg: IStatusMessage) {
+        console.log(`jacdac: ${msg.data.status}`, msg.data)
+        this.sendAck(msg)
+    }
+
     private handleSaveTextMessage(msg: ISaveTextMessage) {
         const { name, data } = msg.data
-        const fileName = findUniqueFileName(this.options.fileBrowserFactory, name)
+        const extname = PathExt.extname(name)
+        const basename = name.substr(0, name.length - extname.length).replace(/\d+$/, '');
+        const fileName = findUniqueFileName(this.options.fileBrowserFactory, basename, extname)
         this.options.contents.save(fileName, {
             type: 'file',
             format: 'text',
@@ -88,11 +120,12 @@ export class JACDACWidget extends IFrame {
         console.log(`jacdac: ack`, msg)
     }
 
-    private sendAck(msg: IMessage, error: any) {
+    private sendAck(msg: IMessage, data?: any, error?: any) {
         this.postMessage({
             ackId: msg.id,
             data: {
                 status: error ? "error" : "success",
+                data,
                 error
             }
         } as IAckMessage)
@@ -100,15 +133,16 @@ export class JACDACWidget extends IFrame {
 
     private postMessage(msg: IMessage) {
         msg.id = "jl:" + Math.random()
+        msg.source = "host"
         const url = new URL(this.options.url)
-        const target = `${url.protocol}${url.hostname}`
-        window.parent.postMessage(msg, target)
+        const target = `${url.protocol}${url.host}`
+        this.iframe.contentWindow.postMessage(msg, target)
         return Promise.resolve();
     }
 }
 
 // walks the current folder in the filebrowser for a unique new file name
-function findUniqueFileName(fileBrowserFactory: IFileBrowserFactory, name: string = "data") {
+function findUniqueFileName(fileBrowserFactory: IFileBrowserFactory, base: string, ext: string) {
     const fileBrowserModel = fileBrowserFactory.defaultBrowser.model;
     const fileModels = toArray(fileBrowserModel.items());
     const fileModelMap: { [path: string]: Contents.IModel; } = {};
@@ -119,7 +153,7 @@ function findUniqueFileName(fileBrowserFactory: IFileBrowserFactory, name: strin
     let fileName = '';
     do {
         const nb = fileNameIndex.toString().padStart(3, "0")
-        fileName = `${fileBrowserModel.path}/${name}-${nb}.csv`;
+        fileName = `${fileBrowserModel.path}/${base}-${nb}${ext}`;
         fileNameIndex++
     } while (!!fileModelMap[fileName]);
     return fileName;
